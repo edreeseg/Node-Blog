8ab43f89202190ba78cf02d0d9192bea7146a1d1
edreeseg <edward.reeseg@gmail.com>
2019-01-22 16:13:41 -0500

    CRUD operations for '/posts' endpoint

--- index.js
+++ index.js
@@ -63,6 +63,63 @@ server.delete('/users/:id', (req, res) => {
         .catch(err => res.status(500).json({ error: 'There was an error while trying to delete user.' }));
 });
 
+server.get('/posts', (req, res) => {
+    post.get()
+        .then(posts => res.json({ posts }))
+        .catch(err => res.status(500).json({ error: 'There was an error while retrieving the posts.' }));
+});
+
+server.get('/posts/:id', (req, res) => {
+    const id = req.params.id;
+    post.get(id)
+        .then(post => res.json(post))
+        .catch(err => JSON.stringify(err) === '{}'  // Does it only return this when there is no post by that ID?  Or is this the only error message?
+            ? res.status(404).json({ error: 'There is no post with that ID' })
+            : res.status(500).json({ error: 'There was an error while retrieving that post.' }));
+});
+
+server.post('/posts', (req, res) => {
+    const [userId, text] = [req.body.userId, req.body.text];
+    if (!userId || !text) return res.status(400).json({ error: 'Please provide both an existing user ID and text content for post.' });
+    user.get(userId)
+        .then(user => {
+            if (!user) return res.status(404).json({ error: 'No user associated with provided user ID.' });
+            post.insert({ userId, text })
+                .then(info => {
+                    post.get(info.id)
+                        .then(post => res.status(201).json(post))
+                        .catch(err => res.status(500).json({ error: 'Error occurred after adding post.' }));
+                })
+                .catch(err => res.status(500).json({ error: 'Error occurred while adding post.' }));
+        })
+        .catch(err => res.status(500).json({ error: 'Error occured while retrieving user infromation.' }));
+});
+
+server.put('/posts/:id', (req, res) => {
+    const id = req.params.id;
+    if (Object.keys(req.body).some(key => key !== 'text')) return res.status(400).json({ error: 'Request body must only include updated text value.' });
+    post.update(id, req.body)
+        .then(n => {
+            if (!n) return res.status(404).json({ error: 'Could not find post by provided ID.' });
+            post.get(id)
+                .then(post => res.json(post))
+                .catch(err => res.status(500).json({ error: 'Error occurred after updating post.' }));
+        })
+        .catch(err => res.status(500).json({ error: 'Error occurred while updating post.' }));
+});
+
+server.delete('/posts/:id', (req, res) => {
+    const id = req.params.id;
+    post.remove(id)
+        .then(n => {
+            if (!n) return res.status(404).json({ error: 'Could not find post by provided ID.' });
+            post.get()
+                .then(posts => res.json({ posts }))
+                .catch(err => res.status(500).json({ error: 'Error occurred after deleting post.' }));
+        })
+        .catch(err => res.status(500).json({ error: 'Error occurred while attempting to delete post.' }));
+});
+
 server.listen(5000, () => {
     console.log(`Server listening on port ${port}.`);
 });
